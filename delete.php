ac950742ad49ed0d9fbb48dd177f6aa467d01fba
jstockwin <jstockwin@gmail.com>
2015-10-06 10:13:47 +0100

    Removed commented out code

--- delete.php
+++ delete.php
@@ -5,9 +5,7 @@
 	sec_session_start();
 	if (login_check($mysqli) == "admin") {
 	$target_dir = "uploads/".$_GET['dir'];
-	//$target_dir = "uploads/";
 	$target_file = $target_dir . $_POST['fileToDelete'];
-	//throw new Exception($target_file);
 	if (is_dir($target_file)) {
 		rmdir($target_file);
 	}else{
