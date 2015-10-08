5a949c932701de6fc27e2307fe30dba4e55ad5ca
jstockwin <jstockwin@gmail.com>
2015-10-08 11:01:47 +0100

    Loaded icons over https

--- includes/head.php
+++ includes/head.php
@@ -27,7 +27,7 @@ if ($_SERVER['HTTPS'] != "on") {
 
 <head>
 	<link rel="stylesheet" type="text/css" media="all" href="/style.css">
-	<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<link rel="icon" href="/images/favicon.ico" type="image/x-icon">
 	<meta name="theme-color" content="#2f68a1">
